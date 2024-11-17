9b4f99d5897a705d23d716e31cade14ba905aabc
Deivide Maciel <31144383+deivomaciel@users.noreply.github.com>
2024-11-17 18:29:09 -0300

    Update login.php

--- api/login.php
+++ api/login.php
@@ -5,11 +5,11 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 // Conectando ao banco de dados
-require_once __DIR__ . '/../vendor/vlucas/autoload.php';  // Caminho absoluto baseado na localização do script
+// require_once __DIR__ . '/../vendor/vlucas/autoload.php';  // Caminho absoluto baseado na localização do script
 
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
 
 // $host = $_ENV['DB_HOST'];
 // $dbname = $_ENV['DB_NAME'];
